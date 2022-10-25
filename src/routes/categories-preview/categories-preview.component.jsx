56665bd5a04a51d35d0bf78366903b8bdc802dc6
shafoo98 <67111399+shafoo98@users.noreply.github.com>
2022-10-25 22:01:49 +0600

    "Added sub routes for shops"

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,20 @@
+import { Fragment, useContext } from "react";
+
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+
+import { CategoriesContext } from "../../context/categories.context";
+
+const CategoriesPreview = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  return (
+    <Fragment className="category-preview-container">
+      {Object.keys(categoriesMap).map((key) => {
+        const products = categoriesMap[key];
+        return <CategoryPreview key={key} title={key} products={products} />;
+      })}
+    </Fragment>
+  );
+};
+
+export default CategoriesPreview;
